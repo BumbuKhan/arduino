bb682f2cd232730c4151d48b726ad4c555ce8523
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-12-16 18:32:56 +0400

    Now LED has state

--- Button controlled LED/sketch_dec16a/sketch_dec16a.ino
+++ Button controlled LED/sketch_dec16a/sketch_dec16a.ino
@@ -1,19 +1,21 @@
 int ledpin = 11;
 int inpin = 7;
 int val;
+int curstate;
 
 void setup () {
   pinMode(inpin, INPUT);
   pinMode(ledpin, OUTPUT);
+  curstate = 0;
 }
 
 void loop () {
   val = digitalRead(inpin);
 
-  if (val == LOW) {
-    digitalWrite(ledpin, LOW);
-  } else {
-    digitalWrite(ledpin, HIGH);
+  if (val) {
+    curstate = !curstate;
   }
+
+  digitalWrite(ledpin, curstate);
 }
 
